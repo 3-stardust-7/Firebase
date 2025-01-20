533eb0b418911f155cc767195e4db68c45e51da3
Kirubha V <vkirubha7@gmail.com>
2025-01-20 21:05:50 +0530

    Set up firestore and fetched data,ie read

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,53 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Auth from "./components/auth";
+import { db } from "./config/firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  const moviesCollectionRef = collection(db, "movies");
+
+  useEffect(() => {
+    const getMovie = async () => {
+      //SET THE MOVIE LIST
+      try {
+        //READ THE DATA
+        const data = await getDocs(moviesCollectionRef);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        //console.log(data);
+        //console.log(filteredData);
+        setMovieList(filteredData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getMovie();
+  }, []);
+
   return (
     <>
       <div className="App">
         <Auth />
-        
+        <div>
+          {movieList.map((movie) => (
+            <div key={movie.id}>
+              <h1 style={{ color: movie.receivedAnOscar ? "green" : "red" }}>
+                {movie.title}
+              </h1>
+              <p>Date: {movie.releaseDate}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
+
 }
 
 export default App;
--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -11,7 +11,7 @@ const Auth = () => {
   const [password, setPassword] = useState("");
 
   //console.log(auth?.currentUser?.email);
-  console.log(auth?.currentUser?.photoURL);
+  //console.log(auth?.currentUser?.photoURL);
 
   const signIn = async () => {
     try {
